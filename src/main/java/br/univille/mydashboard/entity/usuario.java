4c83cc11ddf6b963d7bde73f0ee65a9c6a52b365
RAFAEL MACHADO <rafael.machado.1@univille.br>
2022-05-03 20:24:04 -0300

    entidades

---
+++ src/main/java/br/univille/mydashboard/entity/usuario.java
@@ -0,0 +1,54 @@
+package br.univille.mydashboard.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private String sexo;
+    private String telefone;
+    @Temporal(value = TemporalType.DATE)
+    private Date nascimento;
+
+    public long getId() {
+        return id;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
