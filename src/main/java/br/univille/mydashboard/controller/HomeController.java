0192fc347c78dfaaf0d3f1067af0a50220f88b90
RAFAEL MACHADO <rafael.machado.1@univille.br>
2022-05-03 19:29:23 -0300

    first one

---
+++ src/main/java/br/univille/mydashboard/controller/HomeController.java
@@ -0,0 +1,15 @@
+package br.univille.mydashboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        return "gzus";
+    }
+}
