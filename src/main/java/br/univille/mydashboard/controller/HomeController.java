00deac3cbf08190088171ad3b565303e710c6507
Rafael Machado <62083559+Rafael-Machaddo@users.noreply.github.com>
2022-07-05 14:58:30 -0300

    update login/home/usuario

--- src/main/java/br/univille/mydashboard/controller/HomeController.java
+++ src/main/java/br/univille/mydashboard/controller/HomeController.java
@@ -1,24 +1,18 @@
 package br.univille.mydashboard.controller;
 
-import java.util.HashMap;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 @RequestMapping({"/","/home"})
 public class HomeController {
-    
+   
     @GetMapping()
     public ModelAndView index(){
-        var nome = "Serjão";
-        var sobrenome = "Berranteiro";
-        HashMap<String,String> mapa_de_dados = new HashMap<>();
-        mapa_de_dados.put("nomenatela", nome);
-        mapa_de_dados.put("sobrenomenatela", sobrenome);
         return new 
-            ModelAndView("home/index",mapa_de_dados);
+        ModelAndView("home/index");
     }
 }
